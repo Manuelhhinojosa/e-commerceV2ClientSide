f7f30331b525c80baf0f2b3b3ac8b6087a02d5c8
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2025-09-16 11:36:16 -0400

    feat(router): add folder structure and UserLayout component
    - Create components and pages folders in src
    - Add admin, cart, common, layout, and products subfolders under components
    - Set up React Router in App.jsx
    - Create UserLayout component in layout folder
    - Import UserLayout component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+// React router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import UserLayout from "./components/layout/UserLayout";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
