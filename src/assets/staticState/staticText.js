b16565a5d615a2c0a891c9aa3b70f6748c33b789
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2025-09-16 12:31:33 -0400

    feat(content): add static text for Topbar and update contact info
    - Create staticState folder in assets and staticText.js file
    - Add object containing all Topbar text and import into Topbar component
    - Update contact link from phone number to email as per client request

---
+++ src/assets/staticState/staticText.js
@@ -0,0 +1,9 @@
+const topbarText = {
+  centerText: "We ship worlwide - Fast and reliable shipping!",
+  metaLink: "https://wwww.facebook.com",
+  instagramLink: "https://www.instagram.com",
+  xLink: "https://www.x.com",
+  contactEmail: "mangud.hinojosa@gmail.com",
+};
+
+export default topbarText;
--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -2,29 +2,49 @@
 import { IoLogoInstagram } from "react-icons/io";
 import { RiTwitterXLine } from "react-icons/ri";
 import { TbBrandMeta } from "react-icons/tb";
+import { TfiEmail } from "react-icons/tfi";
+
+// Static state (text)
+import topbarText from "../../assets/staticState/staticText";
 
 const Topbar = () => {
   return (
-    <div className="bg-[#e82e0e] text-white">
+    <div className="bg-red text-white">
       <div className="container mx-auto flex justify-between items-center py-3">
         <div className="hidden md:flex item-center space-x-4">
-          <a href="#" className="hover:text-gray-300">
+          <a
+            href={topbarText.metaLink}
+            target="_blank"
+            className="hover:text-gray-300"
+          >
             <TbBrandMeta className="h-5 w-5" />
           </a>
-          <a href="#" className="hover:text-gray-300">
+          <a
+            href={topbarText.instagramLink}
+            target="_blank"
+            className="hover:text-gray-300"
+          >
             <IoLogoInstagram className="h-5 w-5" />
           </a>
-          <a href="#" className="hover:text-gray-300">
+          <a
+            href={topbarText.xLink}
+            target="_blank"
+            className="hover:text-gray-300"
+          >
             <RiTwitterXLine className="h-5 w-5" />
           </a>
         </div>
 
         <div className="text-sm text-center flex-grow">
-          <span>We ship worlwide - Fast and reliable shipping!</span>
+          <span>{topbarText.centerText}</span>
         </div>
         <div className="text-sm hidden md:block">
-          <a href="tel:+4161231234" className="hover:text-gray-300">
-            +1 (416) 123-1234
+          <a
+            href={`mailto:${topbarText.contactEmail}`}
+            target="_blank"
+            className="hover:text-gray-300"
+          >
+            <TfiEmail className="h-5 w-5" />
           </a>
         </div>
       </div>
